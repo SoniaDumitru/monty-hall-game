25de476c051b1e9453895e3b747cca6ced646c50
Sonia Dumitru <soni.dumitru@gmail.com>
2019-12-16 13:30:18 -0600

    final commit

--- src/monty_hall.js
+++ src/monty_hall.js
@@ -3,32 +3,29 @@ let gamesArray = [];
 let randomNumber;
 
 function selectDoor() {
-  return randomNumber = Math.floor(Math.random() * 3); //choose a number from 0 to 2
+  return randomNumber = Math.floor(Math.random() * 3);
 }
 
 function generateGames() {
   for (let i=0; i<gamesToPlay; i++) {
-    gamesArray.push(selectDoor())
+    gamesArray.push(selectDoor());
   }
   return gamesArray;
 }
+
 generateGames();
 
 function play(switchDoor) {
-  let winningDoor = '';
-  let randomGuess = '';
-  let totalTimesWon = 0;
-
+  let count = 0;
   for (let i = 0; i<gamesToPlay; i++) {
-    winningDoor = gamesArray[i]; //pick up the winning door from the gamesArray
-    randomGuess = randomNumber; //random number: 0, 1 or 2
-    if(randomGuess === winningDoor && !switchDoor) { //if I guess the winningDoor from the first try and I never switch, I win
-      totalTimesWon++;
-    } else if (randomGuess !== winningDoor && switchDoor) { //if I don't guess the door from the first try but I switch
-      totalTimesWon++;
+    if(randomNumber === gamesArray[i] && !switchDoor) {
+      count++;
+    } else if (randomNumber !== gamesArray[i] && switchDoor) {
+      count++;
     }
   }
-return totalTimesWon
+return count;
 }
 
-console.log("If you play "  + gamesToPlay + " games & you don't switch doors, you win:", play(false), " times. If you switch doors, you win: ", play(true), " times." );
+console.log(`If you play ${gamesToPlay} games and switch doors you have ${play(true)/100} % chances to win `)
+console.log(`If you play ${gamesToPlay} games and don't switch doors you have ${play(false)/100} % chances to win `)
